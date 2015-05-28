d58dd42886eff13b3f57761d7168b19517afd1ac
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-05-28 12:59:41 +0200

    More tests in bits reader

--- src/bit.rs
+++ src/bit.rs
@@ -9,7 +9,7 @@ pub struct BitRead<R> where R: Read {
     /// The current cached data being read. This is right-shifted when you call `read`.
     data: u16,
 
-    /// Number of bits remaining to read in `current_byte`. Must be between 0 and 7.
+    /// Number of bits remaining to read in `data`. Must be between 0 and 7.
     bits: u8,
 }
 
@@ -51,8 +51,7 @@ impl<R> BitRead<R> where R: Read {
 
     /// Aligns to the next byte and returns the wrapper reader.
     pub fn byte_align_unwrap(self) -> R {
-        // FIXME: incorrect
-        assert!(self.bits <= 7);
+        debug_assert!(self.bits <= 7);
         self.inner
     }
 
@@ -74,6 +73,7 @@ impl<R> BitRead<R> where R: Read {
 #[cfg(test)]
 mod tests {
     use std::io::Cursor;
+    use std::io::Read;
     use super::BitRead;
 
     #[test]
@@ -110,5 +110,40 @@ mod tests {
         let data = Cursor::new(vec![0b01001110, 0b11011000]);
         let mut data = BitRead::new(data);
         data.read(9).unwrap();
+    }
+
+    #[test]
+    fn byte_align() {
+        let data = Cursor::new(vec![0b01001110, 0xaa]);
+
+        let mut data = BitRead::new(data);
+        assert_eq!(data.read(2).unwrap(), 0b10);
+
+        let data = data.byte_align_unwrap();
+        assert_eq!(data.bytes().next().unwrap().unwrap(), 0xaa);
+    }
+
+    #[test]
+    fn byte_align_0() {
+        let data = Cursor::new(vec![0xcc, 0xaa]);
+
+        let mut data = BitRead::new(data);
+        assert_eq!(data.read(0).unwrap(), 0);
+
+        let data = data.byte_align_unwrap();
+        let mut data = data.bytes();
+        assert_eq!(data.next().unwrap().unwrap(), 0xcc);
+        assert_eq!(data.next().unwrap().unwrap(), 0xaa);
+    }
+
+    #[test]
+    fn byte_align_8() {
+        let data = Cursor::new(vec![0b01001110, 0xaa]);
+
+        let mut data = BitRead::new(data);
+        assert_eq!(data.read(8).unwrap(), 0b01001110);
+
+        let data = data.byte_align_unwrap();
+        assert_eq!(data.bytes().next().unwrap().unwrap(), 0xaa);
     }
 }
